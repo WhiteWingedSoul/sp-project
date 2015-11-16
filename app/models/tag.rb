8bbff97d78168c560965b764aaa222952e9c1cd5
White <cooro1994@gmail.com>
2015-11-16 05:39:20 +0000

    cant search

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,14 @@ class Tag < ActiveRecord::Base
   has_many :posts
   has_many :tag_have
   has_many :tag_want
+  
+  def self.get_tag(post, tag_type)
+      current_tag_have = []
+      all_tag_have = post.send("#{tag_type}").all
+      all_tag_have.each do |a|
+        tbi = find(a.tag)
+        current_tag_have << tbi
+      end
+      return current_tag_have
+  end
 end
